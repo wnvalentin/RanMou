57a6d239b6c772c4e434bad0d2b50da8a5e36411
王宁 <wangning@zy-cast.com>
2020-05-11 19:38:26 +0800

    迁入.Net项目

---
+++ Projects/RanMouStudy_Net/Generic/GenericMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class GenericMethod
+    {
+        //public T MyProperty { get; set; }
+
+        public static void Show<T>(T param)
+        {
+            Console.WriteLine($"这是{typeof(GenericMethod).Name}, 参数={param.GetType().Name}, value={param}");
+        }
+    }
+}
---
+++ Projects/RanMouStudy_Net/RanMouStudy_Net/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generic;
+
+namespace RanMouStudy_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommonMethod.ShowInt(9);
+            CommonMethod.ShowString("haha");
+            CommonMethod.ShowObject(8);
+            CommonMethod.ShowObject(new int[] { 0, 1 });
+            Console.WriteLine();
+
+            Console.WriteLine(typeof(List<int>));
+            Console.WriteLine(typeof(List<>));
+            Console.WriteLine(typeof(Dictionary<,>));
+            Console.WriteLine(typeof(List<string>));
+
+            GenericMethod.Show<int>(7);
+            Console.WriteLine();
+
+
+            ArrayList al = new ArrayList();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+
+    
+}
